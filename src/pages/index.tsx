734754eb9b636d557d4033848b8ab2176cb2f0dd
金子海斗 <spawn@kanekokaitonoMacBook.local>
2020-11-28 15:37:54 +0900

    トップページとブログの日付部分の更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,23 +9,9 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <img
-        src="/vercel-and-notion.png"
-        height="85"
-        width="250"
-        alt="Vercel + Notion"
-      />
-      <h1>My Notion Blog</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
+      <img src="/kanekoya.png" height="85" width="250" alt="金子屋ロゴ" />
+      <h1 className="mb-4 text-green-500 text-3xl">金子屋</h1>
+      <h2>「やりたい」のその先へ</h2>
 
       <Features />
 
