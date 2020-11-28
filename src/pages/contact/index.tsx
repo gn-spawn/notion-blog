de3c23adcf0df8932ebae73f42caae6e34be451c
金子海斗 <spawn@kanekokaitonoMacBook.local>
2020-11-28 15:37:36 +0900

    contact pageを分けた

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,13 +1,13 @@
-import Header from '../components/header'
-import ExtLink from '../components/ext-link'
+import Header from '../../components/header'
+import ExtLink from '../../components/ext-link'
 
-import sharedStyles from '../styles/shared.module.css'
-import contactStyles from '../styles/contact.module.css'
+import sharedStyles from '../../styles/shared.module.css'
+import contactStyles from '../../styles/contact.module.css'
 
-import GitHub from '../components/svgs/github'
-import Twitter from '../components/svgs/twitter'
-import Envelope from '../components/svgs/envelope'
-import LinkedIn from '../components/svgs/linkedin'
+import GitHub from '../../components/svgs/github'
+import Twitter from '../../components/svgs/twitter'
+import Envelope from '../../components/svgs/envelope'
+import LinkedIn from '../../components/svgs/linkedin'
 
 const contacts = [
   {
@@ -36,16 +36,9 @@ export default () => (
   <>
     <Header titlePre="Contact" />
     <div className={sharedStyles.layout}>
-      <div className={contactStyles.avatar}>
-        <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
-      </div>
+      <h1 style={{ marginTop: 0 }}>お問い合わせ</h1>
 
-      <h1 style={{ marginTop: 0 }}>Contact</h1>
-
-      <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
-      </div>
+      <div className={contactStyles.name}>Kaito Kaneko</div>
 
       <div className={contactStyles.links}>
         {contacts.map(({ Comp, link, alt }) => {
